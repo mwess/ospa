9df538cd372eddb7271abd68588bb887cb0d14f5
Maxi Wess <maxwess@googlemail.com>
2018-08-28 00:06:50 +0200

    added Hungarian to REQUIRE, started with README

--- src/ospa.jl
+++ src/ospa.jl
@@ -9,7 +9,8 @@ using Statistics
 using Random
 
 export ospa_kmeans, ospa_barycenter,
-       ospa_dist, Pointcloud
+       ospa_dist, Pointcloud,
+       generateData
        #mean_initialization, sample_from_mean_initialization,
        #ospa_barycenter_sol2
 
@@ -20,5 +21,6 @@ include("mean_barycenter_initialization.jl")
 include("recursive_barycenter_initialization.jl")
 include("cluster_initialization.jl")
 include("kmeans.jl")
+include("testdata.jl")
 
 end # module
